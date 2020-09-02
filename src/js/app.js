972616a298e7bbf68b1ce84e7c5b98ac4a02f080
Elionai Melo <elionai.melo@lais.huol.ufrn.br>
2020-09-02 10:58:30 -0300

    correção para ipad na pg index

--- src/js/app.js
+++ src/js/app.js
@@ -8,10 +8,10 @@ $(document).on("ready", function () {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          centerMode: true,
+          centerPadding: "25px",
         },
       },
       {
